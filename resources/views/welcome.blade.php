b9fd4643cb1c1c483e24ba7b40d4bb731344b5c2
Tiago Oliveira <tiagoliveira555@gmail.com>
2024-02-02 19:16:15 -0300

    adding livewire

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,15 +14,9 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="bg-slate-950 antialiased">
-
-
-    <nav class="border-gray-200 bg-white dark:bg-gray-900">
+<body class="antialiased">
+    <nav class="border-gray-900">
         <div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">Flowbite</span>
-            </a>
             <button data-collapse-toggle="navbar-default" type="button"
                 class="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
                 aria-controls="navbar-default" aria-expanded="false">
@@ -33,6 +27,7 @@
                         d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
             </button>
+            <img src="imgs/logo.png" alt="">
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul
                     class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
@@ -41,27 +36,38 @@
                             class="block rounded bg-blue-700 px-3 py-2 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
                             aria-current="page">Home</a>
                     </li>
-                    <li>
-                        <a href="#"
-                            class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">About</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">Services</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">Pricing</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">Contact</a>
-                    </li>
                 </ul>
             </div>
         </div>
     </nav>
 
+    <!-- drawer init and toggle -->
+    <div class="text-center">
+
+        <button type="button" data-drawer-target="drawer-cart" data-drawer-show="drawer-cart"
+            data-drawer-placement="right" aria-controls="drawer-cart">
+            <x-icons.shopping-cart class="h-8 w-8" />
+        </button>
+    </div>
+
+    <!-- drawer component -->
+    <div id="drawer-cart"
+        class="fixed right-0 top-0 z-40 h-screen w-[370px] translate-x-full overflow-y-auto bg-black p-4 transition-transform md:w-[440px]"
+        tabindex="-1" aria-labelledby="drawer-cart-label">
+        <button type="button" data-drawer-hide="drawer-cart" aria-controls="drawer-cart"
+            class="absolute end-2.5 top-2.5 inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white">
+            <svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+            <span class="sr-only">Close menu</span>
+        </button>
+
+    </div>
+    </div>
+
+    <livewire:count />
 </body>
 
 </html>
