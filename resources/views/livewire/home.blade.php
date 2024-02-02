f15cff7fe8e4c9930c6283c3dc1c6a7a09c91fc8
Tiago Oliveira <tiagoliveira555@gmail.com>
2024-02-02 20:25:13 -0300

    configured tailwind.config

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,7 @@
 <div>
-    <h1>Home</h1>
+    <h1 class="text-3xl text-base-gray-100">Home</h1>
+
+    <div class="h-36 w-36 bg-base-gradient">
+
+    </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,33 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
-        "./node_modules/flowbite/**/*.js"
       ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+        },
+        colors: {
+            base: {
+                'gray-100': '#A1A1A1',
+                'gray-200': '#676767',
+                'gray-300': '#2A2A2A',
+                'black-100': '#1A1A1A',
+                'black-200': '#0B0B0B',
+                'green-100': '#1E7F14',
+                'green-200': '#165F0F',
+            }
+        },
+        backgroundImage: {
+            'base-gradient': "linear-gradient(45deg, #165F0F 0%, #165F0F66 100%)",
+        }
+    },
   },
-  plugins: [
-        require('flowbite/plugin')
-    ],
+  plugins: [],
 }
 
